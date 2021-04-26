5ef50c061e764b3336bb141ae14a51c64109014d
Iván <ivantorrepista1@gmail.com>
2021-04-26 22:15:29 +0200

    Segundo cambio

--- IvanPruebaGit/src/prueba.java
+++ IvanPruebaGit/src/prueba.java
@@ -4,6 +4,8 @@ public class prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Buenos dias");
+		
+		System.out.println("Buenas noches");
 	}
 
 }
